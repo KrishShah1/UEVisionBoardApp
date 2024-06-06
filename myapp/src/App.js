376d64dfff84f3e0b987108d52e8ec08b2aadfbc
Krish <krishshah8590@gmail.com>
2024-06-06 13:25:33 -0700

    Changing app.js and seeing deployment

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,9 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="vision-board">
+        <h1>My Vision Board</h1>
+        <div className="vision-item">
+          <img src="image1.jpg" alt="Vision 1" />
+          <p>My first vision</p>
+        </div>
+        <div className="vision-item">
+          <img src="image2.jpg" alt="Vision 2" />
+          <p>My second vision</p>
+        </div>
+        <div className="vision-item">
+          <img src="image3.jpg" alt="Vision 3" />
+          <p>My third vision</p>
+        </div>
+      </div>
     </div>
   );
 }
