fc64590160f8b86359ec2b0c83ff55443227a769
shreyasjd777 <shreyasjd777@gmail.com>
2024-09-03 22:08:16 -0700

    cleaned up the code

--- myapp/src/components/CameraPage.js
+++ myapp/src/components/CameraPage.js
@@ -24,7 +24,6 @@ const CameraPage = ({ Confirm }) => {
     retakeButton.classList.add('hidden');
     confirmButton.classList.add('hidden');
     captureButton.classList.remove('hidden');
-    // setSelectedTheme('/images/theme3.png');
     setSelectedSelfie('');
   }
 
@@ -112,7 +111,6 @@ const CameraPage = ({ Confirm }) => {
             )}
             <img className='overlay-theme' src={selectedTheme} key={selectedTheme}></img>
             <FaRegCircle className='capture-button' onClick={startCountdown}/>
-            {/* <svg className='capture-button' onClick={startCountdown} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(255,255,255,1)"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20Z"></path></svg> */}
             {countdown !== null && countdown > 0 && (<div className="countdown-animation">{countdown}</div>)}
           </div>
           <button className='hidden side retake' onClick={reset}>Retake</button>
--- myapp/src/components/StickerPage.js
+++ myapp/src/components/StickerPage.js
@@ -3,18 +3,13 @@ import '../styles/StickerPage.css';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import { IoIosCloseCircleOutline } from "react-icons/io";
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import Draggable from 'react-draggable';
 
 
 const StickerPage = ({ selfie, theme, Restart }) => {
 
     const sectionRef = useRef();
-    // const [pdf, setPdf] = useState(null);
     const [showPopup, setShowPopup] = useState(false);
-    const [showRestart, setShowRestart] = useState(false);
-    const [email, setEmail] = useState('');
     const [stickers, setStickers] = useState([]);
 
     const importImages = (r) => {
@@ -35,9 +30,6 @@ const StickerPage = ({ selfie, theme, Restart }) => {
         const img = new Image();
         img.src = stickerSrc;
         img.onload = () => {
-            const imgWidth = img.width;
-            const imgHeight = img.height;
-
             // Add the new sticker to the array, adjusting by half the width and height
             setStickers((prevStickers) => [
                 ...prevStickers,
@@ -53,117 +45,53 @@ const StickerPage = ({ selfie, theme, Restart }) => {
     const clear = () => {
         setStickers([])
     }
-    
-    // const screenshotAndPopup = async() => {
-    //     const screenshotElement = sectionRef.current;
-    //     const canvas = await html2canvas(screenshotElement);
-    //     const image = canvas.toDataURL('image/png');
-
-    //     // Create a PDF and add the image to it
-    //     // Use 'tabloid' for 11" x 17" format
-    //     const tempPdf = await new jsPDF({orientation: 'portrait', unit: 'in', format: 'tabloid'});
-    //     const pdfWidth = tempPdf.internal.pageSize.getWidth();
-    //     const pdfHeight = tempPdf.internal.pageSize.getHeight();
-    //     tempPdf.addImage(image, 'PNG', 0, 0, pdfWidth, pdfHeight);
-    //     const output = tempPdf.output('blob');
-    //     const pdfUrl = URL.createObjectURL(output);
-    //     setPdf(pdfUrl);
-
-        
-
-    //     setShowPopup(true);
-    // }
-
-    // const PrintAndEmail = async() => {
-    //     if (email) {
-    //         const formData = new FormData();
-    //         formData.append('email', email);
-    //         formData.append('file', pdf, 'document.png');
-        
-    //         try {
-    //             const response = await fetch('/send-email', {
-    //                 method: 'POST',
-    //                 body: formData,
-    //             });
-
-    //             if (response.ok) {
-    //                 alert('Email sent successfully!');
-    //             } else {
-    //                 alert('Failed to send email.');
-    //             }
-    //         } catch (error) {
-    //             console.error('Error sending email:', error);
-    //             alert('An error occurred while sending the email.');
-    //         }   
-    //     }
-
-    //     if (pdf) {
-    //         const iframe = document.createElement('iframe');
-    //         iframe.style.position = 'absolute';
-    //         iframe.style.width = '0';
-    //         iframe.style.height = '0';
-    //         iframe.style.border = 'none';
-    //         iframe.src = pdf;
-
-    //         document.body.appendChild(iframe);
-
-    //         // Print the content of the iframe
-    //         iframe.onload = () => {
-    //             iframe.contentWindow.focus();
-    //             iframe.contentWindow.print();
-                
-    //             // Clean up
-    //             document.body.removeChild(iframe);
-    //             URL.revokeObjectURL(pdf);
-    //         };
-    //     }
-
-    //     setTimeout(() => {setShowRestart(true)}, 2000);
-    // }
-
+  
     const handleClose = () => {
         setShowPopup(false);
     };
 
     const screenshotAndPrint = async() => {
+        console.log("hello")
         const screenshotElement = sectionRef.current;
         const canvas = await html2canvas(screenshotElement);
         const image = canvas.toDataURL('image/png');
 
+        while (!image) {
+            console.log('not image')
+        }
+
         // Create a PDF and add the image to it
         // Use 'tabloid' for 11" x 17" format
-        const tempPdf = await new jsPDF({orientation: 'portrait', unit: 'in', format: 'tabloid'});
+        const tempPdf = new jsPDF({orientation: 'portrait', unit: 'in', format: 'tabloid'});
         const pdfWidth = tempPdf.internal.pageSize.getWidth();
         const pdfHeight = tempPdf.internal.pageSize.getHeight();
         tempPdf.addImage(image, 'PNG', 0, 0, pdfWidth, pdfHeight);
         const output = tempPdf.output('blob');
         const pdf = URL.createObjectURL(output);
+
+        console.log('3')
         
-        if (pdf) {
-            const iframe = document.createElement('iframe');
-            iframe.style.position = 'absolute';
-            iframe.style.width = '0';
-            iframe.style.height = '0';
-            iframe.style.border = 'none';
-            iframe.src = pdf;
+        const iframe = document.createElement('iframe');
+        iframe.style.position = 'absolute';
+        iframe.style.width = '0';
+        iframe.style.height = '0';
+        iframe.style.border = 'none';
+        iframe.src = pdf;
 
-            document.body.appendChild(iframe);
+        document.body.appendChild(iframe);
 
-            // Print the content of the iframe
-            iframe.onload = () => {
-                iframe.contentWindow.focus();
-                iframe.contentWindow.print();
-                
-                // Clean up
-                document.body.removeChild(iframe);
-                URL.revokeObjectURL(pdf);
-            };
-        }
+        // Print the content of the iframe
+        iframe.onload = () => {
+            iframe.contentWindow.focus();
+            iframe.contentWindow.print();
+            
+            // Clean up
+            document.body.removeChild(iframe);
+            URL.revokeObjectURL(pdf);
+        };
 
         setTimeout(() => {setShowPopup(true)}, 1000);
     }
-
-    const foo = () => {}
 
     return (
         <>
@@ -189,14 +117,10 @@ const StickerPage = ({ selfie, theme, Restart }) => {
                         <div className='overlay-container' ref={sectionRef}>
                             {selfie ? (
                                 <img className='final-selfie' src={selfie}></img>
-                            ) : (
-                                foo()
-                            )}
+                            ) : (() => {})}
                             {theme ? (
                                 <img className='final-theme' src={theme}></img>
-                            ) : (
-                                foo()
-                            )}
+                            ) : (() => {})}
                             <div className='sticker-canvas' onDrop={handleDrop} onDragOver={handleDragOver}>
                                     {stickers.map((sticker, index) => (
                                         <Draggable onMouseDown={(e) => {e.preventDefault()}}>
@@ -221,7 +145,6 @@ const StickerPage = ({ selfie, theme, Restart }) => {
                                 onChange={(e) => setEmail(e.target.value)} 
                                 placeholder="email (optional)"
                             /> */}
-                            {/* <button className='button green print' onClick={PrintAndEmail}>Print</button> */}
                             <button className='button green restart' onClick={Restart}>Restart</button>
                         </div>
                     </div>
